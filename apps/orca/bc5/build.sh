214eb6a239f05f7c8adc364bb6bb9928fa15463b
Thomas Green <thomas.green@bristol.ac.uk>
2026-03-30 23:51:17 +0100

    Add system and example.

---
+++ apps/orca/bc5/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+        
+# Set safety options
+set -eu
+
+# Clone Spack version
+git clone --depth=2 --branch releases/v1.1 https://github.com/spack/spack.git
+
+# Souce environment
+. spack/share/spack/setup-env.sh
+
+# Clone Buildit configuration
+#git clone https://github.com/i/buildit.git
+# Use local copy
+ln -sf ../../../ ./buildit
+
+# Disable local config
+export SPACK_DISABLE_LOCAL_CONFIG=true
+export SPACK_USER_CACHE_PATH=$PWD/spack-cache
+
+# Create local directory "myenv" for Spack environment
+spack env create -d myenv
+
+# Activate environment
+spack env activate ./myenv
+
+# Initialise environment
+spack config add -f buildit/config/bc5/v1.1/packages.yaml
+spack config add config:build_jobs:8
+spack config add view:true
+spack config add concretizer:unify:true
+spack config add concretizer:reuse:false
+
+# Add local repo to environment
+spack repo add ./buildit/repo/v1.1/spack_repo/isamrepo
+
+# Add application
+spack add orca@avx2-6.1.1
+
+# Check dependencies
+spack concretize
+
+# Install application
+spack install
+
+# Unload and reload environment (to load new view)
+spack env deactivate && spack env activate ./myenv
+
+# Check application is found
+which orca
+
+# Deactivate environment
+spack env deactivate
+
